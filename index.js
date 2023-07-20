7bda5e9426f99098a4bef14850e10058668e5223
Vedant Tiwari <vedantyadu@gmail.com>
2023-07-20 23:50:13 +0530

    changed url

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ connect_to_mongoDB()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://chat-frontend-ccha.onrender.com/',
   credentials: true
 }))
 
